51e71c6375a0332d38db22e99475c2a549acb484
Ramani <dhopathiramani@example.com>
2024-09-18 09:59:57 +0530

    first commit1

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, UploadFile, File, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
